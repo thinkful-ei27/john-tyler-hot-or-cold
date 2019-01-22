f545408f850f2a00f3c9e54d205b285c6bd5357f
John Young <john.young@spreadstreet.io>
2019-01-22 15:07:29 -0500

    Added form and feedback state

--- src/Game.js
+++ src/Game.js
@@ -11,18 +11,37 @@ export default class Game extends Component {
             feedback: 'Make a guess!',
             currentGuess: 50,
             secretGuess: Math.floor(Math.random()*100)+1,
-            guesses: Array(9).fill(Math.floor(Math.random()*100)+1)
+            guesses: []
         }
+        this.setGuess = this.setGuess.bind(this);
     }
+
+    giveFeedback(n) {
+        const secretGuess = this.state.secretGuess;
+        const fb = (r) => this.setState({feedback: r});
+        return (secretGuess - n === 0) ? fb('Winner!')
+        : (secretGuess - n < 10) ? fb('Hot!')
+        : (secretGuess - n < 20) ? fb('Kinda hot')
+        : (secretGuess - n < 30) ? fb('Warm')
+        : fb('Cold as ice');
+    }
+
+    setGuess(currentGuess) {
+        this.setState({currentGuess})
+        this.giveFeedback(currentGuess);
+    }
+
   render() {
-      const guesses = this.state.guesses;
+    const {guesses, feedback} = this.state;
     return (
         <main className="container center">
             <header>
                 <h1>Hot or Cold</h1>
             </header>
-            <Feedback feedback={this.state.feedback}/>
-            <Form />
+            <Feedback feedback={feedback}/>
+            <Form 
+                setGuess={this.setGuess}
+            />
             <Count guesses={guesses}/>
             <Guesses guesses={guesses}/>
             <button>New Game?</button>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-function Form() {
+function Form(props) {
+  let inputValue;
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.setGuess(inputValue.value);
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
           Enter a guess: <br />
-          <input type="text"></input>
+          <input 
+            type="text" 
+            ref={input => inputValue = input}
+            // onChange={}
+          />
       </label>
       <input type="submit" value="Submit"></input>
     </form>
